db03795b381103a5f104e017a6d92779e94bb95d
johnaresgado95 <64903984+johnaresgado95@users.noreply.github.com>
2022-06-07 09:27:52 +0800

    Update index.js
    Added process

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 
 //INIT
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const DB = "mongodb+srv://johnarenson:johnarenson@cluster0.cgp1g.mongodb.net/?retryWrites=true&w=majority";
 
